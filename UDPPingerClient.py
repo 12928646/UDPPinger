80f10ccece3b238139de7c94d92799ab3aaeef96
12928646 <31385497+12928646@users.noreply.github.com>
2017-10-07 01:31:19 +1100

    Add files via upload

--- UDPPingerClient.py
+++ UDPPingerClient.py
@@ -1,18 +1,27 @@
 import time
 from socket import *
 
-for pings in range(10):
+# Sequence number of the ping message
+pings = 0
+#for pings in range(10):
+while pings < 10:
+    pings +=1
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.settimeout(1)
-    message = 'test'
-    addr = ("127.0.0.1", 12000)
+    message = "Ping"
+    address = ("127.0.0.1", 12000)
 
     start = time.time()
-    clientSocket.sendto(message, addr)
+    clientSocket.sendto(message, address)
     try:
         data, server = clientSocket.recvfrom(1024)
         end = time.time()
         elapsed = end - start
-        print ('%s %d %d' % (data, pings, elapsed))
+        print "Reply from " + address[0] + ":" , data, pings
+        #print('%s %d %d' % (data, pings, elapsed)
+        print "RTT: " + str(end - start)
+
     except timeout:
         print ('Request timed out')
+
+
